f14566dcd66d0dcb79d01c00207ca2bb46f208f1
سمير <161032439+Smr021@users.noreply.github.com>
2024-10-10 14:31:18 +0000

    Salvar

--- Atividade/main.py
+++ Atividade/main.py
@@ -1,11 +1,9 @@
 import os
-from sqlalchemy import create_engine, Column, String, Integer 
+from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Criando banco de dados.
-db = create_engine('sqlite:///meubanco.db') #criando banco SQLite
 
-# CREATE DATABASE meubanco.
+db = create_engine('sqlite:///meubancoBD.db')
 
 # Conecxão com banco de dados.
 Session = sessionmaker (bind=db)
@@ -36,52 +34,35 @@ Base.metadata.create_all(bind=db)
 os.system('cls || clear')
 
 # Salvar no bando de dados.
-# usuario = Usuario('Marta','marta@gmail.com','123')        
-usuario = Usuario(nome='Maria',email='maria@gmail.com',senha='123')
-session.add(usuario)
-session.commit()
+# usuario = Usuario('Marta','marta@gmail.com','123')    
+for i in range(2):
+    nome = input('Digite seu nome: ')
+    email = input('Digite seu email: ')
+    senha = input('Digite seu senha: ')
+
+    usuario = Usuario(nome=nome,email=email,senha=senha)
+    session.add(usuario)
+    session.commit()
 
 # Mostrando conteúdo do banco de dados.
+print('\nListando  usuário no banco de dados.')
 lista_usuarios = session.query(Usuario).all()
 
+for usuario in lista_usuarios:
+    print(f'{usuario.id} - {usuario.nome} - {usuario.email}')
+
+# Deletando um usuário.
+usuario = session.query(Usuario).filter_by(email='joao@gmail.com').first()
+session.delete(usuario)
+session.commit()
+print('\nDeletando usuário no banco de dados.')
+
+
+print('\nListando  usuário no banco de dados.')
+lista_usuarios = session.query(Usuario).all()
 for usuario in lista_usuarios:
     print(f'{usuario.id} - {usuario.nome} - {usuario.email}')
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Persistir - Banco de Dados
-# SGBD
-# SQL - Relacionais
-# Comandos Criar banco de dados, Tabelas...
-# SELECT * FROM CLIENTES
-# INSERT, CREATE TABLE, CREATE DATABASE...
-
-
-# Backend - SQL
-
-# ORM
-
-# INSERT, UPDATE, SELECT *
-
-# pip install sqlalchemy - ferramenta que permite usar o ORM
-
-# bind uma conexão com alguma coisa, no caso com banco de dados
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Banco_De_Dados_Python10deOutubro
+
+# Persistir - Banco de Dados
+# SGBD
+# SQL - Relacionais
+# Comandos Criar banco de dados, Tabelas...
+# SELECT * FROM CLIENTES
+# INSERT, CREATE TABLE, CREATE DATABASE...
+
+
+# Backend - SQL
+
+# ORM
+
+# INSERT, UPDATE, SELECT *
+
+# pip install sqlalchemy - ferramenta que permite usar o ORM
+
+# bind uma conexão com alguma coisa, no caso com banco de dados
+
+# markdaw
+## Atividade:
+### Instruçoes:
+- R.A.
+- Nome
+- Idade
